67af3e7063e2f666d1502798e2d6603c39e95a49
Guillermo Albarello <galbarello@gmail.com>
2011-03-23 14:19:22 -0300

    cambio en la formula de puntos trunco el valor.

--- FileProccesor/Services/Helpers/HelperPuntos.cs
+++ FileProccesor/Services/Helpers/HelperPuntos.cs
@@ -9,18 +9,19 @@ namespace FileProccesor.Services.Helpers
     {
         public static int GetPuntos(int empresa,DateTime comprobante, double monto)
         {
-            var puntos = 0.00;
+            var puntos = 0d;
 
             var reglas=ActiveRecordBase<ReglasDto>.FindAll()
                 .Where(x => x.Key.CodEmpresa == empresa)
                 .Where(x => x.Key.Sucursal == HelperSucursal.GetSucursal())
                 .Where(x=>x.Key.FechaInicio<=comprobante)
+                .Where(x=>x.FechaFin==null || x.FechaFin> comprobante )
                 .ToList();
 
             foreach (var regla in reglas)
             {
-                var modificador = monto*regla.Multiplicador;
-                puntos = ++modificador;
+                var modificador = monto * Convert.ToDouble(regla.Multiplicador);
+                puntos = puntos+modificador;
             }
 
             return (int) puntos;
